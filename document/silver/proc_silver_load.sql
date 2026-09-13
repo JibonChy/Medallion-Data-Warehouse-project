a87d5cd3806f496b541772520265053c7bdf3a21
Jibon Chowdhury - Data Engineer <jibonchowdhury48000@gmail.com>
2026-09-13 22:12:33 +0600

    Create proc_silver_load.sql

---
+++ document/silver/proc_silver_load.sql
@@ -0,0 +1,274 @@
+/*
+===========================================
+    Silver Layer Loading Procedure
+===========================================
+
+Introduction:
+    This stored procedure automates the transformation and loading
+    of CRM and ERP data from the Bronze layer into the Silver layer.
+    It applies cleaning, standardization, and enrichment rules to
+    ensure data quality and consistency.
+
+Purpose:
+    - Truncate Silver tables before each load to avoid duplication.
+    - Clean CRM customer data (trim names, normalize marital status/gender).
+    - Standardize CRM product data (derive category IDs, normalize product lines).
+    - Validate CRM sales details (fix dates, recalculate sales, correct prices).
+    - Clean ERP customer data (normalize IDs, gender values).
+    - Standardize ERP location data (map country codes to names).
+    - Preserve ERP product category data with consistent naming.
+    - Capture execution start/end timestamps for each table and batch.
+    - Provide error handling with detailed messages for debugging.
+
+Execute Procedure:
+    EXEC proc_silver_layer_loding;
+*/
+
+
+
+CREATE OR ALTER PROCEDURE proc_silver_layer_loding AS
+
+BEGIN
+	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
+	BEGIN TRY
+		SET @batch_start_time = GETDATE();
+		PRINT '=======================================================';
+		PRINT '>>> START: CRM Silver Layer Loading';
+		PRINT '=======================================================';
+
+
+
+		SET @start_time = GETDATE();
+		PRINT '>>>Table Truncation: silver.crm_cust_info';
+		TRUNCATE TABLE silver.crm_cust_info;
+		PRINT '>>>Data Insertion Process: silver.crm_cust_info';
+		INSERT INTO silver.crm_cust_info 
+		(
+				cst_id,
+				cst_key,
+				cst_firstname,
+				cst_lastname,
+				cst_marital_status,
+				cst_gndr,
+				cst_create_date
+		)
+		SELECT 
+			cst_id,
+			cst_key,
+			TRIM(cst_firstname) AS cst_firstname,
+			TRIM(cst_lastname) AS cst_lastname,
+			CASE
+				WHEN cst_marital_status = 'S' THEN 'Single'
+				WHEN cst_marital_status = 'M' THEN 'Married'
+				ELSE 'n/a'
+			END AS cst_marital_status,
+			CASE
+				WHEN cst_gndr = 'F' THEN 'Female'
+				WHEN cst_gndr = 'M' THEN 'Male'
+				ELSE 'n/a'
+			END AS cst_gndr,
+			cst_create_date
+		FROM 
+		(
+			SELECT 
+				*,
+				ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS chacking_duplicate
+			FROM bronze.crm_cust_info
+		)t
+		WHERE chacking_duplicate = 1;
+		SET @end_time = GETDATE();
+		PRINT '>>>Total Execution Duration (seconds):' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR);
+
+
+
+		SET @start_time = GETDATE();
+		PRINT '>>>Table Truncation: silver.crm_prd_info';
+		TRUNCATE TABLE silver.crm_prd_info;
+		PRINT '>>>Data Insertion Process: silver.crm_prd_info';
+		INSERT INTO silver.crm_prd_info 
+		(
+			prd_id,
+			cat_id,
+			prd_key,
+			prd_nm,
+			prd_cost,
+			prd_line,
+			prd_start_dt,
+			prd_end_dt
+		)
+		SELECT
+			prd_id,
+			SUBSTRING(REPLACE(prd_key, '-', '_'), 1, 5) AS cat_id,
+			SUBSTRING(REPLACE(prd_key, '-', '_'),7, LEN(prd_key)) AS prd_key,
+			prd_nm,
+			COALESCE(prd_cost, 0) AS prd_cost,
+			CASE prd_line
+				WHEN 'M' THEN 'Mexo'
+				WHEN 'R' THEN 'Rivo'
+				WHEN 'S' THEN 'Sero'
+				WHEN 'T' THEN 'Tivo'
+				ELSE 'n/a'
+			END prd_line,
+			prd_start_dt,
+			prd_end_dt
+		FROM bronze.crm_prd_info;
+		SET @end_time = GETDATE();
+		PRINT '>>>Total Execution Duration (seconds):' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR);
+
+
+
+		SET @start_time = GETDATE();
+		PRINT '>>>Table Truncation: silver.crm_sales_details';
+		TRUNCATE TABLE silver.crm_sales_details;
+		PRINT '>>>Data Insertion Process: silver.crm_sales_details';
+		INSERT INTO silver.crm_sales_details
+		(
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
+			sls_order_dt,
+			sls_ship_dt,
+			sls_due_dt,
+			sls_sales,
+			sls_quantity,
+			sls_price
+		)
+
+		SELECT 
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
+			TRY_CAST(sls_order_dt AS DATE) AS sls_order_dt,
+			TRY_CAST(sls_ship_dt AS DATE) AS sls_ship_dt,
+			TRY_CAST(sls_due_dt AS DATE) AS sls_due_dt,
+			CASE 
+				WHEN sls_sales IS NULL OR sls_sales != sls_quantity * ABS(sls_price)
+				THEN sls_quantity * ABS(sls_price)
+				ELSE sls_sales
+			END AS sls_sales,
+			sls_quantity,
+			CASE 
+				WHEN sls_price IS NULL OR sls_price <=0
+				THEN sls_sales / NULLIF(sls_quantity, 0)
+				ELSE ABS(sls_price)
+			END AS sls_price
+		FROM bronze.crm_sales_details;
+		SET @end_time = GETDATE();
+		PRINT '>>>Total Execution Duration (seconds):' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR);
+
+
+		SET @batch_end_time = GETDATE();
+		PRINT '=======================================================';
+		PRINT '>>> END:  CRM Silver Loading Completed Successfully';
+		PRINT '>>>Total CRM Silver Layer Loading Duration (seconds): ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR);
+		PRINT '=======================================================';
+
+
+		PRINT'';
+		PRINT'-----------------------------------------------------------';
+		PRINT'';
+
+
+
+		SET @batch_start_time = GETDATE();
+		PRINT '=======================================================';
+		PRINT '>>> START: ERP Silver Layer Loading';
+		PRINT '=======================================================';
+
+
+		SET @start_time = GETDATE();
+		PRINT '>>>Table Truncation: silver.erp_cust_az12';
+		TRUNCATE TABLE silver.erp_cust_az12;
+		PRINT '>>>Data Insertion Process: silver.erp_cust_az12';
+		INSERT INTO silver.erp_cust_az12 
+		(
+			CID,
+			BDATE,
+			GEN
+		)
+		SELECT 
+			CASE 
+				WHEN CID LIKE 'NAS%' THEN SUBSTRING(CID, 4, LEN(CID))
+				ELSE CID
+			END AS CID,
+			BDATE AS BDATE,
+			CASE 
+				WHEN GEN = 'F' THEN 'Female'
+				WHEN GEN = 'M' THEN 'Male'
+				WHEN GEN = '' THEN 'n/a'
+				WHEN GEN IS NULL THEN 'n/a'
+				ELSE GEN
+			END AS GEN
+		FROM bronze.erp_cust_az12;
+		SET @end_time = GETDATE();
+		PRINT '>>>Total Execution Duration (seconds):' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR);
+
+
+
+		SET @start_time = GETDATE();
+		PRINT '>>>Table Truncation: silver.erp_loc_a101';
+		TRUNCATE TABLE silver.erp_loc_a101;
+		PRINT '>>>Data Insertion Process: silver.erp_loc_a101';
+		INSERT INTO silver.erp_loc_a101
+		(
+			CID,
+			CNTRY
+		)
+		SELECT
+			REPLACE(CID, '-', '') AS CID,
+			CASE 
+				WHEN CNTRY = 'DE' THEN 'Germany'
+				WHEN CNTRY = 'USA' THEN 'United States'
+				WHEN CNTRY = 'US' THEN 'United States'
+				WHEN CNTRY = '' THEN 'n/a'
+				WHEN CNTRY IS NULL THEN 'n/a'
+				ELSE CNTRY
+			END AS CNTRY
+		FROM bronze.erp_loc_a101;
+		SET @end_time = GETDATE();
+		PRINT '>>>Total Execution Duration (seconds):' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR);
+
+
+
+		SET @start_time = GETDATE();
+		PRINT '>>>Table Truncation: silver.erp_px_cat_g1v2';
+		TRUNCATE TABLE silver.erp_px_cat_g1v2;
+		PRINT '>>>Data Insertion Process: silver.erp_px_cat_g1v2';
+		INSERT INTO silver.erp_px_cat_g1v2
+		(
+			id,
+			cat,
+			subcat,
+			maintainance
+		)
+		SELECT 
+			ID, 
+			CAT,
+			SUBCAT,
+			MAINTENANCE
+		FROM bronze.erp_px_cat_g1v2;
+		SET @end_time = GETDATE();
+		PRINT '>>>Total Execution Duration (seconds):' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR);
+
+
+
+		SET @batch_end_time = GETDATE();
+		PRINT '=======================================================';
+		PRINT '>>> END:  ERP Silver Layer Loading Completed Successfully';
+		PRINT '>>>Total ERP Silver Layer Loading Duration (seconds): ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR);
+		PRINT '=======================================================';
+
+	END TRY
+
+	BEGIN CATCH
+		PRINT '===========================================';
+		PRINT '❌ ERROR OCCURRED DURING EXECUTION';
+		PRINT 'Error Message   : ' + ERROR_MESSAGE();   --- main error text
+		PRINT 'Error Number    : ' + CAST(ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error State     : ' + CAST(ERROR_STATE() AS NVARCHAR);
+		PRINT 'Error Line      : ' + CAST(ERROR_LINE() AS NVARCHAR);
+		PRINT '===========================================';
+	END CATCH;
+END;
+
+EXEC proc_silver_layer_loding;
